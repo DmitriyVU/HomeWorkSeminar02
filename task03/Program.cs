bf76fec27d47ba3ae2f1a2ebad420be360f6e974
DmitriyVU <Dmitriy.vladimirovich.u@gmail.com>
2023-01-08 13:55:30 +0300

    add task03

---
+++ task03/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 15: Напишите программу, которая принимает 
+на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Clear();
+
+string[] week = { "monday", "tuesday", "wednsday", "thursday", "friday", "saturday", "sunday" };
+
+Console.WriteLine("Введите номер дня недели");
+int num = int.Parse(Console.ReadLine()?? "wares");
+
+if (num < 1 || num > 7)
+{
+    Console.WriteLine("Сколько дней в неделе?");
+}
+else if (num > 5)
+{
+    Console.WriteLine($"сегодня {week[num - 1]}, выходной!");
+}
+else
+{
+    Console.WriteLine($"Сегодня {week[num - 1]}, рабочий день");
+}
